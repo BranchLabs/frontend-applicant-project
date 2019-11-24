d84811474b552019749d53f1e0c1bcb0fb80eaf5
Kiril <keiraarts@gmail.com>
2019-11-24 13:35:24 -0500

    save_data()

---
+++ .history/pages/index_20191124133337.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import AppForm from '../components/SubmitForm';
+import Table from '../components/Table';
+import Descriptiom from '../components/Description';
+import { Page } from '@shopify/polaris';
+
+function Home() {
+	return (
+		<Page
+			title='Spreadsheet App'
+			subtitle="For a demo, select C3 and press 'Enter' twice or load data."
+			narrowWidth
+			primaryAction={{ title: 123 }}
+		>
+			<Table />
+			<AppForm />
+			<Descriptiom />
+		</Page>
+	);
+}
+
+export default Home;
